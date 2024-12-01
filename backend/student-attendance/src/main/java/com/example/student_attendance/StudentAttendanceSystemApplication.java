2b3eb60ddeea6d950f04395332abaf82281b4a77
Albion Qerreti <albionqerreti221b@gmail.com>
2024-12-01 15:51:42 +0100

    Basic Directory Structure

---
+++ backend/student-attendance/src/main/java/com/example/student_attendance/StudentAttendanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceSystemApplication.class, args);
+	}
+
+}

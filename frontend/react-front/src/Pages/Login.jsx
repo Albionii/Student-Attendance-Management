e0e9121984280baf6c935938368a650b05c08009
Blend112 <blendelezii04@gmail.com>
2024-12-17 20:51:16 +0100

    ndryshova frontin e log in formes

--- frontend/react-front/src/Pages/Login.jsx
+++ frontend/react-front/src/Pages/Login.jsx
@@ -7,28 +7,28 @@ const LoginForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Email:', email);
-    console.log('Password:', password);
+    cogitnsole.log('Password:', password);
     // Add login logic here (e.g., API call)
   };
 
   return (
     <div
       className="h-screen w-full bg-cover bg-center flex justify-center items-center"
-      style={{ backgroundImage: "url('/background11.jpg')" }}
+      style={{ backgroundImage: "url('public/background11.jpg')" }}
     >
-      <div className="bg-white bg-opacity-75 p-8 rounded-lg shadow-lg w-full max-w-md">
+      <div className="bg-white bg-opacity-65 p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">Login</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm text-gray-600 mb-2">
-              Email
+              ID
             </label>
             <input
               type="email"
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
+              className="w-full px-4 py-2  border border-gray-300 rounded-md"
               required
             />
           </div>
@@ -41,13 +41,13 @@ const LoginForm = () => {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
+              className="w-full px-4 py-2  border border-gray-300 rounded-md"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+            className="w-full py-2 bg-yellow-400 text-white rounded-md hover:bg-red-700 transition"
           >
             Login
           </button>

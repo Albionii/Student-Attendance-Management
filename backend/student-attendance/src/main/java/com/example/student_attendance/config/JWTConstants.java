cd3eff6344ea98235dc6bd8a3a4a6da6f6d90ea1
Abdusamed Beqiri <beqiriabdusamed@gmail.com>
2024-12-20 02:12:19 +0100

    Update JWTConstants.java

--- backend/student-attendance/src/main/java/com/example/student_attendance/config/JWTConstants.java
+++ backend/student-attendance/src/main/java/com/example/student_attendance/config/JWTConstants.java
@@ -1,7 +1,7 @@
 package com.example.student_attendance.config;
 
 public class JWTConstants {
-    public static final String SECRET_KEY = "AlbiAbubuBlendiJavaSteamStreamSmallLarge2XLSMDijekstra";
+    public static final String SECRET_KEY = "AlbiAbubuBlendiJavaSteamStreamSmallLarge2XLSMDijekstraBubi";
     public static final String JWT_HEADER = "Authorization";
 
 }
